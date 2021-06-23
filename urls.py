3fa370cf88b0a1dffa528adb6b86a533fb8c4c6e
HafizhL15 <83531538+HafizhL15@users.noreply.github.com>
2021-06-23 15:20:17 +0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^delete/(?P<delete_id>[0-9]+)$', views.delete, name='delete'),
-	url(r'^update/(?P<update_id>[0-9]+)$', views.update, name='update'),
+	url(r'^delete/(?P<delete_id>[0-9]+)$', views.deletelist, name='delete'),
+	url(r'^update/(?P<update_id>[0-9]+)$', views.updatelist, name='update'),
 	url(r'^create/$',views.create, name='create'),
 	url(r'^$',views.list, name='list'),
 ]
