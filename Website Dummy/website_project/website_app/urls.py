e19b5bd7cbdc4629b833d8749ac4480bb84ec429
fitrailyasa12 <nde.joker12@gmail.com>
2021-07-05 13:04:55 +0700

    Update template
    Mengganti hyperlink

--- Website Dummy/website_project/website_app/urls.py
+++ Website Dummy/website_project/website_app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from . import views
 urlpatterns = [
-    url(r'^home_page_icos/$',views.home_icos),
+    url(r'^/$',views.home_icos),
     url(r'^home_page_icos2021/$',views.home_icos2021),
     url(r'^info_conference2021/$',views.info_conference2021),
     url(r'^regis_conference2021/$',views.regis_conference2021),
